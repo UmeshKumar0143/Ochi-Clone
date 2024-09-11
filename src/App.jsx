c06d8302e107ad01625044d2e0407eb28735d93d
UmeshKumar0143 <149981630+UmeshKumar0143@users.noreply.github.com>
2024-09-12 00:47:06 +0530

    Created the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navbar from './componnets/Navbar'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='w-full h-screen '>
+      <Navbar/>
+    </div>
   )
 }
 

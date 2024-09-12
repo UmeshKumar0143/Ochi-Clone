63a8cac92c0aeea85e538d8247cf5fee16cfa5d2
UmeshKumar0143 <149981630+UmeshKumar0143@users.noreply.github.com>
2024-09-12 22:28:51 +0530

    completed Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './componnets/Navbar'
+import Hero from './componnets/Hero'
 
 const App = () => {
   return (
     <div className='w-full h-screen '>
       <Navbar/>
+      <Hero/>
     </div>
   )
 }
--- src/componnets/Hero.jsx
+++ src/componnets/Hero.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { FaArrowRight } from 'react-icons/fa'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className=''>
+      <div className='mt-20 ml-16'>
+        {["We Create","Eye-Opening","Presentations"].map((item,index)=>{
+         return <div className=' flex items-center'>
+          {index == 1 && <div className='h-[5vw] mt-4 w-[7.5vw] bg-red-500' ></div>}
+         <h1 className='text-[7vw] uppercase font-["Founders Grotesk X- Condensed"] font-bold tracking-tighter  leading-[7vw]'>{item}</h1>
+         </div>
+        })}
+        </div>
+        <div className='Below-Part border-zinc-500 px-5 border-t-[1px] flex py-2 justify-between items-center mt-20'>
+          {["For Public and Private companies", "From the first pitch to IPO"].map((item,index)=><p className='text-sm capitalize  font-normal '>{item}</p>)}
+
+         <div className='buton-div flex  font-["Neue Montreal"] items-center gap-2'> 
+        <button className='px-5 border-2 border-zinc-700 rounded-full uppercase  py-1'>Start the project</button>
+        <button className='-rotate-[45deg] border-zinc-700 border-2  rounded-full py-2 px-2'><FaArrowRight className='font-light text-lg' /></button>
+        </div>
+        </div>
+
+    </div>
   )
 }
 
